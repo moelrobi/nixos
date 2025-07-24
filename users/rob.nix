c0a4e87bffc4f83cc3c204773b7208369caee49b
Robin Möller <robin@moeller.mx>
2025-07-24 22:49:38 +0200

    Fix Formating and change ssh to matchBlocks

--- users/rob.nix
+++ users/rob.nix
@@ -1,58 +1,60 @@
 { pkgs, config, lib, ... }:
 
 {
-  home-manager.users.rob = {
-    home.stateVersion = "25.05";
+    home-manager.users.rob = {
+        home.stateVersion = "25.05";
 
-    programs.home-manager.enable = true;
+        programs.home-manager.enable = true;
 
-    home.username = "rob";
-    home.homeDirectory = "/home/rob";
-    home.keyboard = "de";
+        home.username = "rob";
+        home.homeDirectory = "/home/rob";
+        home.keyboard = "de";
 
-    xdg.userDirs = {
-        enable = true;
-        createDirectories = true;
-    };
+        xdg.userDirs = {
+            enable = true;
+            createDirectories = true;
+        };
 
-    home.packages = with pkgs; [
-      # Add any additional packages you want to install for the user here
-      discord
-      steam
-      spotify
-      vscode
-    ];
+        home.packages = with pkgs; [
+          # Add any additional packages you want to install for the user here
+          discord
+          steam
+          spotify
+          vscode
+        ];
 
-    fonts.fontconfig.enable = true;
+        fonts.fontconfig.enable = true;
 
-    programs.fish.enable = true;
+        programs.fish.enable = true;
 
-    services.flameshot.enable = true;
+        services.flameshot.enable = true;
 
-    dconf = {
-        enable = true;
-        settings = {
-            "org/gnome/desktop/input-sources" = {
-                mru-sources="[('xkb', 'us')]";
-                sources="[('xkb', 'de')]";
+        dconf = {
+            enable = true;
+            settings = {
+                "org/gnome/desktop/input-sources" = {
+	    	        mru-sources="[('xkb', 'de')]";
+                    sources="[('xkb', 'de')]";
+                };
+                "org/gnome/desktop/interface" = {
+                    color-scheme = "prefer-dark";
+                };
             };
-            "org/gnome/desktop/interface" = {
-                color-scheme = "prefer-dark";
+        };
+
+        programs.git = {
+            enable = true; # Ensure the Git program is enabled
+            userEmail = "robin@moeller.mx";
+            userName = "Robin Möller";     
+        };
+
+        programs.ssh = {
+            enable = true;
+            matchBlocks = {
+                "*" = {
+                    identityAgent = "~/.1password/agent.sock";
+                };
             };
         };
     };
-
-    programs.git = {
-       userEmail = "robin@moeller.mx";
-       userName = "Robin Möller";     
-    };
-
-    programs.ssh = {
-           enable = true;
-           extraConfig = ''
-                Host *
-                    IdentityAgent ~/.1password/agent.sock
-          '';
-     };
-  };
 }
