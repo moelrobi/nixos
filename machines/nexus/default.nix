7577ca0e98b0827ddb46a97af883e9c0cd0925bf
Robin Möller <robin@moeller.mx>
2025-07-24 22:49:51 +0200

    Fixate the xserver layout

--- machines/nexus/default.nix
+++ machines/nexus/default.nix
@@ -20,6 +20,7 @@
     };
 
     services.xserver.enable = true;
+    services.xserver.layout = "de";
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
     services.xserver.videoDrivers = [ "nvidia" ];
