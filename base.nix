933ab608fc3ebd88912cf4072f462c0e3b3267f2
Robin Möller <r.moeller@fh-muenster.de>
2025-07-24 14:37:22 +0200

    Remove networkmanager-wireguard plugin from base.nix

--- base.nix
+++ base.nix
@@ -27,7 +27,6 @@
     };
 
     networking.networkmanager.enable = true;
-    networking.networkmanager.plugins = with pkgs; [ networkmanager-wireguard ];
 
     time.timeZone = "Europe/Berlin";
     networking.timeServers = [ "0.pool.ntp.org" "1.pool.ntp.org" "2.pool.ntp.org" ] ++ options.networking.timeServers.default;
