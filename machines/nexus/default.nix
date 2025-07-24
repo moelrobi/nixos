3bd7733b671c8b21d530d7e33260c7047519a610
Robin Möller <r.moeller@fh-muenster.de>
2025-07-24 14:51:05 +0200

    Add xserver configuration for Gnome and NVIDIA support in default.nix

--- machines/nexus/default.nix
+++ machines/nexus/default.nix
@@ -16,5 +16,11 @@
 
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
-    }
+    };
+
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+
 }
