78a7071a119ae3506b36c8bf818adc40709cd969
Robin Möller <robin@moeller.mx>
2025-07-26 21:59:56 +0200

    37 current  2025-07-26 21:59:52  25.05.806796.92c2e04a4755  6.15.7                          *

--- machines/nexus/default.nix
+++ machines/nexus/default.nix
@@ -21,10 +21,6 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  networking.hosts = {
-    "10.0.100.46" = ["web-nix.uwu.tools" "web-nix" "i.uwu.tools"];
-  };
-
   services.xserver.enable = true;
   services.xserver.xkb.layout = "de";
   services.xserver.displayManager.gdm.enable = true;
