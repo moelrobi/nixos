55ee00a4898d51af4fccbb27d5ae7b44ce04a6d0
Robin Möller <robin@moeller.mx>
2025-08-04 12:01:23 +0200

    49 current  2025-08-04 12:01:19  25.05.806796.92c2e04a4755  6.15.7                          *

--- machines/maelle/default.nix
+++ machines/maelle/default.nix
@@ -27,6 +27,13 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
 
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   services.sunshine = {
     enable = true;
     autoStart = true;
